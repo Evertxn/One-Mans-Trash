b4734426164edd69d594bd40164da4da57641697
EVERTON B WEBB <Ev@evertons-mbp.local.valenciacc.edu>
2017-07-11 21:31:28 -0400

    creating post table

--- omt/database/migrations/2017_07_12_010736_create_posts_table.php
+++ omt/database/migrations/2017_07_12_010736_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Post extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,11 @@ class Post extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +27,6 @@ class Post extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('posts');
     }
 }
